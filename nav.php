446e2bb0d38bbb5561c0e7c3b31a84d264d6c609
mikimi <mikimi987654@gmail.com>
2020-04-21 13:01:18 +0200

    egyszerű javítások

--- nav.php
+++ nav.php
@@ -1,8 +1,12 @@
 <div id="nav">
 
 <a href="index.php">Home</a>
-<a href="index.php?M=users$P=login">Login</a>
-<a href="index.php?M=users$P=register">Register</a>
+<?php if (IsUserLoggedIn()): ?>
 	<a href="index.php?P=logout">Logout</a>
+
+<?php else : ?>	
+	<a href="index.php?M=users$P=register">Register</a>
+	<a href="index.php?M=users$P=login">Login</a>
+	<?php endif ?>
 
 </div>
